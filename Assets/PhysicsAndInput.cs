5423b7074661069e62e2b83341d8c2d5ce596369
NevLo <pilleychristian@gmail.com>
2021-04-18 21:21:45 -0700

    Initial commit

---
+++ Assets/PhysicsAndInput.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsAndInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+
+    public Vector3 pos;
+    public Vector3 vel;
+    public Vector3 accl;
+    public float deltaPos = .05F;
+    public float jumpSpeed = 1F;
+    public float height = 2;
+    public float gravity = -.98F;
+    void Start()
+    {
+        pos = Vector3.zero;
+        vel = Vector3.zero;
+        accl = Vector3.zero;
+        accl.y = 0;
+        pos.y = height;
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        getInputs();
+
+        vel = vel + accl * Time.deltaTime;
+        pos = pos + vel * Time.deltaTime;
+        if (pos.y <= height)
+        {
+            pos.y = height;
+            accl.y = 0;
+        }        
+
+        transform.localPosition = pos;
+
+    }
+
+    void getInputs()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+            pos.x -= deltaPos;
+        if (Input.GetKey(KeyCode.RightArrow))
+            pos.x += deltaPos;
+        if (Input.GetKey(KeyCode.UpArrow))
+            pos.z += deltaPos;
+        if (Input.GetKey(KeyCode.DownArrow))
+            pos.z -= deltaPos;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            vel.y = jumpSpeed;
+            accl.y = gravity;
+        }
+    }
+
+}
+
