d1a8fbcb4d8b61710959128fc74e8b0debb0448f
Cade Hockersmith <invent4u369@gmail.com>
2021-04-19 09:15:22 -0700

    Title Screen, Basic UI

---
+++ Assets/UIMgr.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMgr : MonoBehaviour
+{
+    public static UIMgr inst;
+    public void Awake()
+    {
+        inst = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public Text speedLabel;
+    public Text speedText;
+    public Text boostLabel;
+    public Text boostText;
+    public Text healthLabel;
+    public Text healthText;
+    public GameObject player;
+
+    //ScriptableObject test = player.GetComponent<PhysicsAndInput>();
+
+    //GameObject player = Player;
+    // Update is called once per frame
+    void Update()
+    {
+        //speedText.text = (player.GetComponent<PhysicsAndInput>).ToString(); 
+        //boostText.text = ().ToString(); 
+        //healthText.text = ().ToString(); 
+    }
+}
